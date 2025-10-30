695ef0dd92382fff9c3dc7ed97d11d0629f800b5
novryn <senrits@gmail.com>
2025-10-30 11:31:16 +0900

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -1,6 +1,5 @@
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import time
 
 def test_click_login_button(driver):
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "loginBtn")))
+    time.sleep(3)
     driver.find_element(By.ID, "loginBtn").click()
